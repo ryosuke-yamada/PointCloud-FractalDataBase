525d41d78ae6cbefd462274b4a386ed853b507ed
ryosuke.yamada <50004406+ryosuke-yamada@users.noreply.github.com>
2022-03-24 22:56:36 +0900

    Delete header.js

--- header.js
+++
@@ -1,5 +0,0 @@
-document.write('\
-<div>\
-<a href="http://xpaperchallenge.org/cv/" target="_self"><img src="./img/cc_logo_2.png" style="margin-top: 5px; width:100%; height:auto; max-height:100px; max-width:525px;" /></a>\
-</div>\
-');
